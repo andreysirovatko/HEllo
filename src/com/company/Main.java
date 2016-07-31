a95e7e4d10317a23f07537bb6d018af9d135da13
andreysirovatko <asirovatko31@gmail.com>
2016-07-31 20:25:15 +0300

    хелло

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] arg) {
+        System.out.println("Calculate...");
+    int first = Integer.valueOf(arg[0]);
+        int second = Integer.valueOf(arg[1]);
+        int summ = first + second;
+        System.out.println("sum" + summ);
+
+
+    }
+}
